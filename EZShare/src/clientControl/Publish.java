e88b12c591f72efd41c0258326422d9411810d91
sangclee17 <sangclee17@gmail.com>
2017-03-28 22:48:41 +1100

    initial commit

--- EZShare/src/clientControl/Publish.java
+++ EZShare/src/clientControl/Publish.java
@@ -1,5 +1,34 @@
 package clientControl;
 
-public class Publish {
+import bean.ClientJSON;
+import bean.Resource;
+import net.sf.json.JSONObject;
 
+
+public class Publish {
+	
+	private ClientJSON clientJson;
+	
+	public Publish() {
+		clientJson = new ClientJSON();
+	}
+
+	public void setCommand(String command) {
+		this.clientJson.setCommand(command); 
+	}
+	
+	public void setResource(Resource resource) {
+		this.clientJson.setResource(resource);
+	}
+	
+	public void printPublish() {
+		JSONObject publishItems = new JSONObject();
+		
+		publishItems.put("command", clientJson.getCommand());
+		publishItems.put("resource", clientJson.getResource());
+		
+		System.out.println(publishItems);
+	}
 }
+
+ 
