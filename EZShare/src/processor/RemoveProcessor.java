453887e8cc5be29d2ca3c56fbe105674ca4fb205
ltsand <liamtawasanders@hotmail.com>
2017-04-16 18:19:01 +1000

    Add files via upload

--- EZShare/src/processor/RemoveProcessor.java
+++ EZShare/src/processor/RemoveProcessor.java
@@ -1,13 +1,47 @@
 package processor;
-
+import bean.Resource;
 import bean.ClientJSON;
 import net.sf.json.JSONObject;
+import server.Main;
 
 public class RemoveProcessor {
 
-	public JSONObject process(ClientJSON cJSON){
+	public JSONObject process(ClientJSON cJSON)
+	{
 		System.out.println("removeprocessor");
-		return new JSONObject();
 		
+		JSONObject removeStatus = new JSONObject();
+		
+		Resource resource = cJSON.getResource();
+		
+		for(int i = 0; i < Main.resourceList.size(); i++)
+		{
+			boolean found = false;
+			
+			if(resource == null)
+			{
+				removeStatus.put("response", "error");
+				removeStatus.put("errorMessage",  "missing resource");
+			}
+			
+			else{
+				if(Main.resourceList.get(i).getOwner().equals(resource.getOwner()) && Main.resourceList.get(i).getChannel().equals(resource.getChannel()) && Main.resourceList.get(i).geturi().equals(resource.geturi()))
+				{
+					found = true;
+					Resource resourceStored = Main.resourceList.get(i);
+					Main.resourceList.remove(i);
+					removeStatus.put("response", "success");
+					System.out.println(removeStatus);
+				}
+				if(found == false)
+				{
+					removeStatus.put("response", "error");
+					removeStatus.put("errorMessage", "cannot remove resource");
+				}
+				
+			}
+		}
+		return removeStatus;
 	}
 }
+
